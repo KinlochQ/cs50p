3c02f424e7ae0c9b69c38f787abeded25ed0e4ef
Kinloch <102131963+KinlochQ@users.noreply.github.com>
2022-12-23 16:35:33 +0200

    Update test_project.py

--- test_project.py
+++ test_project.py
@@ -1,6 +1,6 @@
-from  project import download_mp4,download_mp3 
+from  project import download_mp4,download_mp3,select_path
 import pytest
-
+import tkinter as tk
 
 def main():
  test_download_mp4()
@@ -14,7 +14,10 @@ def test_download_mp3():
     with pytest.raises(TypeError)as e:
         assert download_mp3("rNDVID")
              
-        
+ def test_select_path(mocked_filedialog, capsys):
+    path_label = tk.Label(text="")
+    select_path(path_label)
+    assert path_label.cget("text") == "./downloads"       
         
         
         
