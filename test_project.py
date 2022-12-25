bd02a6da6eb2e9342ddc14c71af30f39d4664a3c
Kinloch <102131963+KinlochQ@users.noreply.github.com>
2022-12-25 19:11:54 +0200

    Update test_project.py

--- test_project.py
+++ test_project.py
@@ -1,11 +1,15 @@
-from  project import download_mp4,download_mp3,select_path
+from  project import download_mp4,download_mp3
 import pytest
 import tkinter as tk
+from pytest import *
+import unittest
 
 def main():
  test_download_mp4()
  test_download_mp3()
- 
+
+
+  
 def test_download_mp4():
     with pytest.raises(TypeError)as e:
         assert download_mp4("rNDVID")
@@ -13,16 +17,11 @@ def test_download_mp4():
 def test_download_mp3(): 
     with pytest.raises(TypeError)as e:
         assert download_mp3("rNDVID")
-             
- def test_select_path(mocked_filedialog, capsys):
-    path_label = tk.Label(text="")
-    select_path(path_label)
-    assert path_label.cget("text") == "./downloads"       
+
+
+class TestFileDialog(unittest.TestCase):
+
+    def test_dialog_closed(self):
+        filename = ""
         
         
-        
-        
-        
-        
-if __name__ == "__main__":
-    main()
